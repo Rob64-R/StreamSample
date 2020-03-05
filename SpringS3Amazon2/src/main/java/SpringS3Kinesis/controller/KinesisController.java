c054ea4889a6b44e9bf1d5d80ddd597f8b8cf73d
Roberto Rdz <roberto.rdz.m@outlook.com>
2020-03-05 11:46:57 -0600

    Generate file from Stream

--- SpringS3Amazon2/src/main/java/SpringS3Kinesis/controller/KinesisController.java
+++ SpringS3Amazon2/src/main/java/SpringS3Kinesis/controller/KinesisController.java
@@ -18,10 +18,10 @@ public class KinesisController {
 	@Autowired
 	KinesisService kinesisService;
 
-	@GetMapping(value = "/produce/{input}/{fileName}/tostream/{stream}")
-	public ResponseEntity<String> produceToStream(@PathVariable String input, @PathVariable String fileName, @PathVariable String stream) {
+	@GetMapping(value = "/produce/{bucketName}/{fileName}/to/{stream}")
+	public ResponseEntity<String> produceToStream(@PathVariable String bucketName, @PathVariable String fileName, @PathVariable String stream) {
 		
-		String path = "files/"+input +"/"+ fileName;
+		String path = "files/"+ bucketName +"/"+ fileName;
 		File file = new File(path);
 		
 		try {
@@ -33,11 +33,14 @@ public class KinesisController {
 		}
 	}
 	
-	@GetMapping(value = "consume/fromstream/{stream}")
-	public ResponseEntity<String> consumeFromStream( @PathVariable String stream) {
+	@GetMapping(value = "consume/{stream}/to/{bucketName}/{fileName}")
+	public ResponseEntity<String> consumeFromStream( @PathVariable String stream, @PathVariable String bucketName, @PathVariable String fileName) {
+		
+		String path = "files/"+ bucketName +"/"+ fileName;
+		File file = new File(path);
 		
 		try {
-			kinesisService.getRecords(stream);
+			kinesisService.getRecords(stream, file);
 			return new ResponseEntity<String>("Success!", HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
